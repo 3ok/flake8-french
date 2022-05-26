5a41c3c2d2f91c3a427b22671364ae7bcb71c8b7
Reda Bouaida <reda.bouaida@outlook.com>
2022-05-26 14:54:56 +0200

    fix stuff I've been too lazy to fix

--- flake8_french.py
+++ flake8_french.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
+import ast
 import importlib.metadata
-import io
 import re
 import tokenize
 from typing import Any
@@ -9,31 +9,31 @@ from typing import Iterator
 
 
 MSG = "FRE001 Use 'fr' to specify a raw f-string instead of 'rf'"
-_string_re = re.compile("^([^'\"]*)(.*)$", re.DOTALL)
+_string_re = re.compile("^([^'\"]*)")
 
 
-def parse_string_literal(src: str) -> tuple[str, str]:
-    """parse a string literal's source into (prefix, string)."""
+def get_string_prefix(src: str) -> str:
+    """parse a string literal to find its prefix."""
     # Credits to https://github.com/asottile/tokenize-rt
     match = _string_re.match(src)
     assert match is not None
-    return match.group(1), match.group(2)
+    return match[1]
 
 
 class Plugin:
     name = __name__
     version = importlib.metadata.version(__name__)
 
-    def __init__(self, code: str) -> None:
-        self.code = code
+    def __init__(
+        self,
+        tree: ast.AST,
+        file_tokens: list[tokenize.TokenInfo],
+    ) -> None:
+        self.tokens = file_tokens
 
     def run(self) -> Iterator[tuple[int, int, str, type[Any]]]:
-        target = io.StringIO(self.code)
-        target.seek(0)
-
-        tokens = tokenize.generate_tokens(target.readline)
-        for token in tokens:
-            if token.type == 3:  # i.e. string
-                prefix, _ = parse_string_literal(token.string)
+        for token in self.tokens:
+            if token.type == tokenize.STRING:
+                prefix = get_string_prefix(token.string)
                 if prefix == 'rf':
                     yield token.start[0], token.start[1], MSG, type(self)
--- tests/flake8_french_test.py
+++ tests/flake8_french_test.py
@@ -1,8 +1,21 @@
+from __future__ import annotations
+
+import ast
+import io
+import tokenize
+
 from flake8_french import Plugin
 
 
-def results(s):
-    return {'{}:{}: {}'.format(*r) for r in Plugin(s).run()}
+def _tokens(src: str) -> list[tokenize.TokenInfo]:
+    sio = io.StringIO(src)
+    return list(tokenize.generate_tokens(sio.readline))
+
+
+def results(s: str) -> set[str]:
+    tree = ast.parse(s)
+    tokens = _tokens(s)
+    return {'{}:{}: {}'.format(*r) for r in Plugin(tree, tokens).run()}
 
 
 def test_trivial():
@@ -10,9 +23,8 @@ def test_trivial():
 
 
 def test_flaek8_french():
-    src = """a = rf'hello hello world'"""
+    src = "a = rf'hello hello world'"
     msg, = results(src)
     assert msg == (
-        "1:4: FRE001 Use 'fr' to specify "
-        "a raw f-string instead of 'rf'"
+        "1:4: FRE001 Use 'fr' to specify a raw f-string instead of 'rf'"
     )
